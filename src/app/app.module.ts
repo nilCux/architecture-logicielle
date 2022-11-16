b170e48788e5dc37ee81d6941091a0c2c5b1a792
lwerey <laurent.werey@al-enterprise.com>
2022-11-16 12:04:14 +0100

    Add banner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { CanvasComponent } from './canvas/canvas.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-  declarations: [AppComponent, ToolbarComponent, CanvasComponent],
+  declarations: [AppComponent, ToolbarComponent, CanvasComponent, BannerComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
