cb725445a94e18754a3e38cc45bbbf9b836f669d
lwerey <laurent.werey@al-enterprise.com>
2023-01-11 11:54:20 +0100

    Add architecture

---
+++ src/app/services/shapes/line.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { DrawerService } from '../drawer.service';
+import { PropertiesService } from '../properties.service';
+import { ShapeService } from '../shape.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LineService extends DrawerService{
+
+  constructor(canvas: HTMLCanvasElement,ctx: CanvasRenderingContext2D,properties: PropertiesService) { 
+    super(canvas,ctx,properties);
+  }
+  
+  override draw() {
+    this.properties.backgroundColor
+    console.error("foisjoisjoisj")
+  }
+
+
+}
